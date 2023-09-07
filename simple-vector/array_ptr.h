7077aee981f3fd0cc362522fb259fd2f09048322
isackcorn <119417956+isackcorn@users.noreply.github.com>
2023-09-08 00:54:24 +0300

    Add files via upload

---
+++ simple-vector/array_ptr.h
@@ -0,0 +1,76 @@
+#pragma once
+#include <cassert>
+#include <cstdlib>
+#include "algorithm"
+
+template<typename Type>
+class ArrayPtr {
+public:
+    // Инициализирует ArrayPtr нулевым указателем
+    ArrayPtr() = default;
+
+    // Создаёт в куче массив из size элементов типа Type.
+    // Если size == 0, поле raw_ptr_ должно быть равно nullptr
+    explicit ArrayPtr(size_t size) {
+        if (0 == size) {
+            raw_ptr_ = nullptr;
+        } else {
+            Type *arr = new Type[size];
+            raw_ptr_ = &arr[0];
+        }
+    }
+
+    // Конструктор из сырого указателя, хранящего адрес массива в куче либо nullptr
+    explicit ArrayPtr(Type *raw_ptr) noexcept {
+        raw_ptr_ = raw_ptr;
+    }
+
+    // Запрещаем копирование
+    ArrayPtr(const ArrayPtr &) = delete;
+
+    ~ArrayPtr() {
+        delete[] raw_ptr_;
+    }
+
+    // Запрещаем присваивание
+    ArrayPtr &operator=(const ArrayPtr &) = delete;
+
+    // Прекращает владением массивом в памяти, возвращает значение адреса массива
+    // После вызова метода указатель на массив должен обнулиться
+    [[nodiscard]] Type *Release() noexcept {
+        Type *temp = raw_ptr_;
+        raw_ptr_ = nullptr;
+        return temp;
+    }
+
+    // Возвращает ссылку на элемент массива с индексом index
+    Type &operator[](size_t index) noexcept {
+        return raw_ptr_[index];
+    }
+
+    // Возвращает константную ссылку на элемент массива с индексом index
+    const Type &operator[](size_t index) const noexcept {
+        return raw_ptr_[index];
+    }
+
+    // Возвращает true, если указатель ненулевой, и false в противном случае
+    explicit operator bool() const {
+        if (raw_ptr_ == nullptr) return false;
+        return true;
+    }
+
+    // Возвращает значение сырого указателя, хранящего адрес начала массива
+    Type *Get() const noexcept {
+        return raw_ptr_;
+    }
+
+    // Обменивается значениям указателя на массив с объектом other
+    void swap(ArrayPtr &other) noexcept {
+        auto temp = other.raw_ptr_;
+        other.raw_ptr_ = raw_ptr_;
+        raw_ptr_ = temp;
+    }
+
+private:
+    Type *raw_ptr_ = nullptr;
+};
